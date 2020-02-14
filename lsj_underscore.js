4e2af8d4cf82dcf9c5b40fd036681f7300fcf7c6
lee_jjoon <whitour@gmail.com>
2020-02-14 20:37:32 +0900

    add default functions

--- lsj_underscore.js
+++ lsj_underscore.js
@@ -1,6 +1,6 @@
 function _each (list, iter) {
 	for (var i = 0; i < list.length; i++) {
-		iter(list[i]);
+		iter(list[i], i);
 	}
 }
 
@@ -34,6 +34,12 @@ function _reduce (list, iter, memo) {
 	return memo;
 }
 
+function _curryR (fn) {
+	return function (a, b) {
+		return arguments.length === 2 ? fn (a, b) : function (b) { return fn(b, a); };
+	}
+}
+
 function _get (obj, key) {
 	return obj === null ? undefined : obj[key];
 }
@@ -51,4 +57,22 @@ function _pipe () {
 			return fn(memo);
 		}, arg);
 	};
+}
+
+function _go (arg) {
+	var fns = _rest(arguments);
+	return _pipe.apply(null, fns)(arg);
+}
+
+function _rest(list, index) {
+	if (!index || index <= 0) index = 1;
+	var newList = [];
+	for (var i = index;  i < list.length; i++) {
+		newList.push(list[i]);
+	}
+	return newList;
+}
+
+function _length (collection) {
+	return _get(collection, 'length');
 }
